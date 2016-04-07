6285643c57bd3d9453708f8ab72cceba8bdbe12b
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2016-04-07 15:02:39 -0400

    fix(bin): added bin script

--- bin/include-version.js
+++ bin/include-version.js
@@ -1,27 +1,29 @@
+#!/usr/bin/env node
+
 'use strict'
 
-const fs = require('fs')
+var fs = require('fs')
 if (!fs.existsSync('./package.json')) {
   throw new Error('Missing package.json')
 }
-const pkg = require('./package.json')
+var pkg = require('./package.json')
 if (!pkg.version) {
   throw new Error('Missing version in package.json')
 }
 if (!pkg.config) {
   throw new Error('Cannot find config inside package.json')
 }
-const label = 'include-version'
-const myConfig = pkg.config[label]
-const files = typeof myConfig === 'string' ? [myConfig] : myConfig
+var label = 'include-version'
+var myConfig = pkg.config[label]
+var files = typeof myConfig === 'string' ? [myConfig] : myConfig
 if (!Array.isArray(files)) {
   throw new Error('Invalid config for ' + label + ': ' + JSON.stringify(myConfig))
 }
 files.forEach(replaceVersionTag)
 
 function replaceVersionTag (filename) {
-  const tag = /\{\{\s?include\-version\s?\}\}/g
-  const source = fs.readFileSync(filename, 'utf8')
-  const replaced = source.replace(tag, pkg.version)
+  var tag = /\{\{\s?include\-version\s?\}\}/g
+  var source = fs.readFileSync(filename, 'utf8')
+  var replaced = source.replace(tag, pkg.version)
   fs.writeFileSync(filename, replaced, 'utf8')
 }
